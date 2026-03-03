3e42df0ac27cc4d541acea886fc7e808513103bc
KuyangC <bintanghutabarat0204@gmail.com>
2026-03-04 03:37:11 +0700

    feat: add product controller/service/module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [PrismaModule, UsersModule, AuthModule, ProductsModule],
   controllers: [],
 })
 export class AppModule {}
