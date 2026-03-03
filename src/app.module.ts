51e8f49d7e349b7f79d4bf225a5fa053eb35704c
KuyangC <bintanghutabarat0204@gmail.com>
2026-03-03 22:57:41 +0700

    fix: downgrade to prisma 6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma.module';
+import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, UsersModule],
+  controllers: [],
 })
 export class AppModule {}
